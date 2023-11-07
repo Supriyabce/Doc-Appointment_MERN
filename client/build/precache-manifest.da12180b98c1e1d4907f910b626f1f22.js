2e3420d098ec074ffa364f21c3ed35bc319c8114
Supriya Rani <supriyarani30399@gmail.com>
2023-11-07 20:25:44 +0530

    final code
    Mern App Deployment

---
+++ client/build/precache-manifest.da12180b98c1e1d4907f910b626f1f22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "89bf2e03fa1e027fe6b9",
+    "url": "/static/css/main.a39e8c9a.chunk.css"
+  },
+  {
+    "revision": "89bf2e03fa1e027fe6b9",
+    "url": "/static/js/main.89bf2e03.chunk.js"
+  },
+  {
+    "revision": "db4c88facc7fc9ef4aa2",
+    "url": "/static/js/1.db4c88fa.chunk.js"
+  },
+  {
+    "revision": "72febcce829b46814a55",
+    "url": "/static/css/2.8a26ac86.chunk.css"
+  },
+  {
+    "revision": "72febcce829b46814a55",
+    "url": "/static/js/2.72febcce.chunk.js"
+  },
+  {
+    "revision": "15728d77b0b26ee58612",
+    "url": "/static/js/runtime~main.15728d77.js"
+  },
+  {
+    "revision": "b6e7cbf4239bb7f3fceeee752f4ad2fe",
+    "url": "/index.html"
+  }
+];
